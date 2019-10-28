16a3b3bb0e39113ce6fc0e0f9555544c312fdf23
mariollet <mariollet@outlook.fr>
2019-10-28 16:38:04 +0100

    little thing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,26 +1,12 @@
 class HomeController < ApplicationController
 
   def index
-    puts
-    puts "$" * 22
-    puts "INDEX !"
-    puts "$" * 22
-    puts
   end
 
   def team
-    puts
-    puts "$" * 22
-    puts "TEAM !"
-    puts "$" * 22
-    puts
   end
 
   def contact
-    puts
-    puts "$" * 22
-    puts "CONTACT !"
-    puts "$" * 22
-    puts
   end
+
 end
