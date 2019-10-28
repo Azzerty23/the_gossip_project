3a2062991ea6f029f3e7ab880da98a8f1f0766b7
mariollet <mariollet@outlook.fr>
2019-10-28 16:50:44 +0100

    don't touch

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
   root "home#index"
-  
-  get "/(:firstname)", to: "home#index"
 
   get "/team", to: "home#team"
 
   get "/contact", to: "home#contact"
+
+  get "/:firstname", to: "home#index"
 
 end
