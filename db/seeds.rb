14ac76bb45b03d35cd1a6822551fe24dafeb848e
Azzerty23 <azzerty23@hotmail.fr>
2019-10-28 14:31:43 +0100

    add migrations and models : User & Gossip

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+user_array = []
+10.times do
+  u = User.create!(first_name: Faker::Name.first_name)
+  user_array << u
+end
+
+gossip_array = []
+50.times do
+  g = Gossip.create!(title: Faker::Lorem.sentence(word_count: 2, supplemental: false, random_words_to_add: 4),
+                content: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 1),
+                author: user_array.sample)
+end
